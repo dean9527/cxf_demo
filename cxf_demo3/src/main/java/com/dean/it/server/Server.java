3f97124d85808bb9bc39109b9a08bae423d4e7aa
dean9527 <denghongchun123@126.com>
2017-07-11 14:52:25 +0800

    add log interceptor

--- cxf_demo3/src/main/java/com/dean/it/server/Server.java
+++ cxf_demo3/src/main/java/com/dean/it/server/Server.java
@@ -1,10 +1,15 @@
 package com.dean.it.server;
 
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
 
 public class Server {
 	public static void main(String[] args) {
 		JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+		factory.getInInterceptors().add(new LoggingInInterceptor());
+		factory.getOutInterceptors().add(new LoggingOutInterceptor());
+		//注意服务器端setServiceClass这里是实现类不是接口
 		factory.setServiceClass(HelloServiceImpl.class);
 		factory.setAddress("http://127.0.0.1:8080/helloService");
 		factory.create();
